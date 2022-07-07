c95ea6dbff86696ba215cada319e3fa91eafab71
12CUY <82843847+12CUY@users.noreply.github.com>
2022-07-07 14:16:17 -0500

    se agrega el proyecto

---
+++ index_login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../proyecto_2/estilos_vista_css/index_login.css">
+
+     
+    <title>login</title>
+</head>
+
+<body>
+
+<section>
+    <!--bostrap-->
+        <div class="container">
+            <div class="user signinBx">
+                <div class="imgBx"><img src="../proyecto_2/imagenes/foto_1.jpg" /></div>
+                <div class="formBx">
+                    <form>
+                        <h2>INICIAR SESION</h2>
+                        <input type="text" placeholder="Usuario." />
+                        <input type="password" placeholder="Contraseña." />
+                        <input type="submit" value="Ingresar" />
+                        <p class="signup">
+                            ¿No tienes una cuenta?
+                            <a href="#" onclick="toggleForm();"> Registrate.</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+            <div class="user signupBx">
+                <div class="formBx">
+                    <form>
+                        <h2>CREAR CUENTA</h2>
+                        <input type="text" placeholder="Nombre del Usuario">
+                        <input type="text" placeholder="Correo Electronico">
+                        <input type="password" placeholder="Contraseña">
+                        <input type="password" placeholder="Confirmar contraseña">
+                        <input type="submit" value="Registrate">
+                        <p class="signup">
+                            ¿Ya tienes una cuenta?
+                            <a href="#" onclick="toggleForm();"> Iniciar Sesion.</a>
+                        </p>
+                    </form>
+                </div>
+                <div class="imgBx"><img src="../proyecto_2/imagenes/foto_2.jpg" /></div>
+            </div>
+        </div>
+    </section>
+    <script>
+        function toggleForm() {
+            section = document.querySelector('section');
+            container = document.querySelector('.container');
+            container.classList.toggle('active');
+            section.classList.toggle('active');
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ vistas/vista_2.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>control - view luz y electrodomestico </title>
+
+    <link rel="stylesheet" href="../estilos_vista_css/vista_estilo_1.css">
+
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+</head>
+<body id="body">
+    <!--encabezado responsive-->
+    <header>
+        <div class="icon__menu">
+            <i class="fas fa-bars" id="btn_open"></i>
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+
+        <div class="name__page">
+            <i class="fab fa-youtube"></i>
+            <h4>593 XDXDXD</h4>
+        </div>
+
+        <div class="options__menu">	
+
+            <a href="vista_1.php" class="selected">
+                <div class="option">
+                    <i class="fas fa-home" title="Inicio"></i>
+                    <h4>Inicio</h4>
+                </div>
+            </a>
+
+            <a href="vista_2.php">
+                <div class="option">
+                    <i class="far fa-file" title="Portafolio"></i>
+                    <h4>vista</h4>
+                </div>
+            </a>
+            
+            <a href="vista_3.php">
+                <div class="option">
+                    <i class="fas fa-video" title="Cursos"></i>
+                    <h4>sistema de prueba</h4>
+                </div>
+            </a>
+
+            <a href="vista_4.php">
+                <div class="option">
+                    <i class="far fa-sticky-note" title="Blog"></i>
+                    <h4>blog direcional</h4>
+                </div>
+            </a>
+
+            <a href="vista_5.php">
+                <div class="option">
+                    <i class="far fa-id-badge" title="Contacto"></i>
+                    <h4>Contacto - id numero - ubicacion</h4>
+                </div>
+            </a>
+
+        </div>
+
+    </div>
+
+    <section>
+           <div class="contenedor">
+        <div class="oso">
+            <span></span>
+            <div class="content">
+                <h2>Titulo Numero 1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsa et repellat alias nulla quae aspernatur consequuntur, </p>
+                <a href="#">ingresar A</a>
+            </div>
+        </div>
+        <div class="oso">
+            <span></span>
+            <div class="content">
+                <h2>Titulo Numero 2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsa et repellat alias nulla quae aspernatur consequuntur, </p>
+                <a href="#">ingresar B</a>
+            </div>
+        </div>
+        <div class="oso">
+            <span></span>
+            <div class="content">
+                <h2>Titulo Numero 3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsa et repellat alias nulla quae aspernatur consequuntur, </p>
+                <a href="#">ingresar C</a>
+            </div>
+        </div>
+    </div> 
+    </section>
+
+
+
+    <script src="../javascrip/index_login.js"></script>
+</body>
+</html>
