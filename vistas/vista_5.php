65c0fe5f29669c48c09da745a50b7536e897be15
mauriciog150 <mra.garcia@yavirac.edu.ec>
2022-07-17 22:52:01 -0500

    mauricio

---
+++ vistas/vista_5.php
@@ -0,0 +1,18 @@
+<?php
+    include('../templates/menu.php');
+    //menu union 
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../estilos_vista_css/responsib_m.css">
+</head>
+<body>
+    
+</body>
+</html>
