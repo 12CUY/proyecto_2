f940c1886980ac4a8646210e1421644497b6070b
mauriciog150 <mra.garcia@yavirac.edu.ec>
2022-07-22 12:39:32 -0500

    mauricio

--- vistas/vista_5.php
+++ vistas/vista_5.php
@@ -8,26 +8,28 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../estilos_vista_css/responsib_m.css">
+    <link rel="stylesheet" href="../estilos_vista_css/imagen_hover.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+
 </head>
 <body>
         <div class="center">
-            <div class="box">
-            </div>
+            <div class="box"></div>
         </div>
     <section>
         <div class="container-portada">
-        <div class="capa-gradient"></div>
-        <div class="container-details">
-            <div class="details">
-                <h1>DOMOTICA</h1>
-                <p> Sistema capaces de automatizar una vivienda o edificación de cualquier tipo, aportando servicios de gestión energética, seguridad, bienestar y comunicación.</p>
-                <button>Ver mas detalles</button>
+            <div class="capa-gradient"></div>
+            <div class="container-details">
+                <div class="details">
+                    <h1>DOMOTICA</h1>
+                    <p> Sistema capaces de automatizar una vivienda o edificación de cualquier tipo, aportando servicios de gestión energética, seguridad, bienestar y comunicación.</p>
+                    <button>Ver mas detalles</button>
+               </div>
             </div>
         </div>
-    </div>
     </section>
 
     
